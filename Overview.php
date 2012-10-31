df9c83fdeec23015a059da6834db337b16bdcf00
Peter Law <PeterJCLaw@gmail.com>
2012-10-31 21:13:22 +0000

    Have the overview show useful dates for the specials.
    This makes it much easier to navigate.

--- Overview.php
+++ Overview.php
@@ -1,15 +1,20 @@
 <style type="text/css">
 	p { margin-bottom: 0; }
 	ul { list-style-type: none; float: left; margin-top: 10px; }
-	li > span { float: left; width: 10ex; margin-top: 15px; text-align: right; padding-right: 2ex; }
+	li > span { float: left; width: 20ex; margin-top: 15px; text-align: right; padding-right: 2ex; }
 </style>
 <?php
 
 $i = 0;
 $debug_info	= "";
 
-function display($image_start, $i, $image) {
-	echo "<li><span>$image_start &mdash; ".($i-1)."</span><a href=\"$image\"><img src=\"".$image.'" height="60" /></a></li>', PHP_EOL;
+function display($image_start, $i, $image, $addon) {
+	if (empty($addon)) {
+		$when = $image_start.' &mdash; '.($i);
+	} else {
+		$when = print_date_for_day($i).$addon;
+	}
+	echo "<li><span>$when</span><a href=\"$image\"><img src=\"".$image.'" height="60" /></a></li>', PHP_EOL;
 }
 
 require_once('Common.inc.php');
@@ -24,27 +29,30 @@ function image_for_day_full($i) {
 		$image = $special_image;
 	}
 
-	return $image;
+	return array($image, $addon);
 }
 
 $col = 1;
 $max_cols = 2;
 $image_start = 0;
-$last_image = image_for_day_full($image_start);
+$image = $addon = '';
+list($last_image, $last_addon) = image_for_day_full($image_start);
 
 echo "<p>Total days: $days_left</p>";
 echo '<ul>';
 for ($i = 0; $i < $days_left; $i++) {
-	$image = image_for_day_full($i);
-	if ($image != $last_image) {
+	list($image, $addon) = image_for_day_full($i);
+	//echo $i, $addon, PHP_EOL;
+	if ($image != $last_image || $last_addon != $addon) {
 		if ( ( $col * $days_left / $max_cols ) <= $i ) {
 			$col++;
 			echo '</ul><ul>';
 		}
-		display($image_start, $i, $last_image);
+		display($image_start, $i-1, $last_image, $last_addon);
 		$last_image = $image;
+		$last_addon = $addon;
 		$image_start = $i;
 	}
 }
-display($image_start, $i, $image);
+display($image_start, $i, $image, $addon);
 echo '</ul>';
